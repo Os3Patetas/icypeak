10fc7755e43849ac798bcb408f3b55663317f1cf
ydkay <caio.consoli@gmail.com>
2021-07-29 21:47:44 -0300

    Basic Layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,31 @@
-import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { RouterModule } from "@angular/router";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./home/home.component";
+import { HeaderComponent } from "./header/header.component";
+import { FooterComponent } from "./footer/footer.component";
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
+  declarations: [AppComponent, HomeComponent, HeaderComponent, FooterComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot([
       {
-        path: 'home',
+        path: "",
         component: HomeComponent,
-        data: { title: 'Home Component', description: '' },
+        data: { title: "Home Component", description: "" },
+      },
+      {
+        path: "",
+        component: HeaderComponent,
+        data: { title: "Header Component", description: "" },
+      },
+      {
+        path: "",
+        component: FooterComponent,
+        data: { title: "Footer Component", description: "" },
       },
     ]),
   ],
